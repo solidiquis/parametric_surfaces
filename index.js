015a0f0a08c0f9b63ec81ee957e835a072cb3f24
Benji Nguyen <benjamin.van.nguyen@gmail.com>
2021-12-27 01:27:06 -0800

    react

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-import react from "react";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import React, { useEffect, useRef, useReducer } from "react";
 import ReactDOM from "react-dom";
-const rust = import("./pkg/index");
 
-function Test() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+const FPS_THROTTLE = 1000 / 60;
+const WASM = import("./pkg/index");
+
+const INITIAL_STATE = {
+  wasm: null,
+  error: false,
+  parametricSurface: null
+};
+
+const ACTIONS = {
+  SET_WASM: "set-wasm",
+  ERROR: "error",
+  SET_PARAMETRIC_SURFACE: "set-parametric-surface"
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.SET_WASM:
+      return { ...state, wasm: action.payload };
+    case ACTIONS.ERROR:
+      console.error(action.payload);
+      return { ...state, error: true };
+    case ACTIONS.SET_PARAMETRIC_SURFACE:
+      console.log("Successfully set parametric surface");
+      return { ...state, parametricSurface: action.payload };
+    default:
+      console.log("Default action on Surface reducer.")
+      return state;
+  }
 }
 
-const canvas = document.getElementById("parametric-surface");
-const gl = canvas.getContext('webgl', { antialias: true });
+function Surface() {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+  const initMount = useRef(true);
+  const initMount2 = useRef(true);
+  const initMount3 = useRef(true);
+  const canvasRef = useRef(null);
 
-window.onload = rust.then(wasm => {
-  if (!gl) {
-    alert("Failed to initialize WebGL");
-    return;
-  }
-
-  let torus;
-  try {
-    torus = new wasm.Torus("parametric-surface");
-  } catch(e) {
-    alert("Failed to initialize parametric surface.");
-    console.error(e);
-    return;
-  }
-
-  const FPS_THROTTLE = 1000 / 60; // 60fps
   var lastDrawTime = -1; 
   var initTime = Date.now();
 
-  function render() {
-    window.requestAnimationFrame(render);
+  const animate = () => {
+    window.requestAnimationFrame(animate);
     
     let currTime = Date.now();
 
@@ -44,8 +56,73 @@ window.onload = rust.then(wasm => {
 
     let elapsedTime = (currTime - initTime) / 1000;
 
-    torus.render(canvas.width, canvas.height, elapsedTime);
-  }
+    state.parametricSurface.render(canvasRef.current.width, canvasRef.current.height, elapsedTime);
+  };
 
-  render();
-});
+  useEffect(async () => {
+    if (!initMount.current) {
+      return;
+    }
+    initMount.current = false;
+
+    try {
+      let wasm = await WASM;
+      dispatch({ type: ACTIONS.SET_WASM, payload: wasm });
+      console.log("Successfully instantiated WASM module.")
+    } catch(e) {
+      dispatch({ type: ACTIONS.ERROR, payload: `Failed to instantiate WASM module with error: ${e}` });
+      return;
+    }
+
+    const gl = canvasRef.current.getContext("webgl", { antialias: true });
+
+    if (!gl) {
+      dispatch({ type: ACTIONS.ERROR, payload: "Failed to intialize WebGL context" });
+      return;
+    } else {
+      console.log("Successfully established WebGL context.");
+    }
+
+  });
+
+  useEffect(() => {
+    if (initMount2.current) {
+      initMount2.current = false;
+      return;
+    }
+
+    try {
+      let torus = new state.wasm.Torus("parametric-surface");
+      dispatch({ type: ACTIONS.SET_PARAMETRIC_SURFACE, payload: torus });
+    } catch(e) {
+      dispatch({ type: ACTIONS.ERROR, payload: `Failed to initialize parametric surface with error: ${e}` });
+      return;
+    }
+  }, [state.wasm]);
+
+  useEffect(() => {
+    if (initMount3.current) {
+      initMount3.current = false;
+      return;
+    }
+
+    animate()
+  }, [state.parametricSurface]);
+
+  return (
+    <>
+      {
+        state.error ?
+        <h1 style={{ color: "white" }}>{"Something went wrong..."}</h1> :
+        <canvas
+          id="parametric-surface"
+          ref={canvasRef}
+          height="600" width="800"
+        ></canvas>
+      }
+    </>
+  )
+}
+
+ReactDOM.render(<Surface />, document.getElementById("root"));
+
